147c3c68302353336edf71aa83202fd1b41baa5a
Nursurtan <arapovnur23@gmail.com>
2021-11-04 22:53:17 +0700

    used a switch statement

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     let softTime = 5
     let mediumTime = 7
     let hardTime = 12
-
+    
+   
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
         let hardness = sender.currentTitle
+//        switch hardness{
+//        case "Soft":
+//            print("softTime")
+//        case "Medium":
+//            print("mediumTime")
+//        case "Hard":
+//            print("hardTime")
+//        default:
+//            print("error")
+//
+//        }
+        if hardness == "Soft"{
+            print(softTime)
+        } else if hardness == "Medium"{
+            print(mediumTime)
+        } else {
+            print(hardTime)
+        }
+       
     }
     
 }
