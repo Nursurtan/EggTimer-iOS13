71b8e7ddc9566092ddf98eeee89894d6cf583409
Nursurtan <arapovnur23@gmail.com>
2021-12-18 22:30:22 +0700

    Added Implementing a Countdown Timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,33 +10,35 @@ import UIKit
 
 
 class ViewController: UIViewController {
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
     
-   
+    @IBOutlet weak var titleLabel: UILabel!
     
+    let eggTimes = ["Soft" : 3, "Medium" : 4, "Hard" : 7]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
+
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle
-//        switch hardness{
-//        case "Soft":
-//            print("softTime")
-//        case "Medium":
-//            print("mediumTime")
-//        case "Hard":
-//            print("hardTime")
-//        default:
-//            print("error")
-//
-//        }
-        if hardness == "Soft"{
-            print(softTime)
-        } else if hardness == "Medium"{
-            print(mediumTime)
-        } else {
-            print(hardTime)
-        }
-       
+        
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        
+        secondsRemaining = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
     }
     
+    @objc func updateTimer() {
+        
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds.")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
 }
